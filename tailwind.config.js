68f87ebd49729e2e38d749fd8b066a07ce0146b6
Gurami Niazashvili <gurami.niazashvili@gmail.com>
2024-02-02 01:03:26 +0400

    feat: init configurations

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      'weather-primary': '#00668a',
+      'weather-secondary': '#004e71',
+    },
+    fontFamily: {
+      Roboto: ['Roboto, sans-serif'],
+    },
+    container: {
+      padding: '2rem',
+      center: true,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+    },
+  },
+  plugins: [],
+};
