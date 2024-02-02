07aa337b3c062307d9f4f08d3b32fc975b67847d
Gurami Niazashvili <gurami.niazashvili@gmail.com>
2024-02-02 14:18:16 +0400

    fix: tailwind configuration and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      'weather-primary': '#00668a',
-      'weather-secondary': '#004e71',
+      colors: {
+        'weather-primary': '#00668a',
+        'weather-secondary': '#004e71',
+      },
     },
     fontFamily: {
       Roboto: ['Roboto, sans-serif'],
