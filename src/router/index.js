20c7f864354808723df18402545dbfa8c4310332
Gurami Niazashvili <gurami.niazashvili@gmail.com>
2024-02-05 14:18:55 +0400

    feat: add title for tab name

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,27 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: 'Home',
+      },
     },
     {
       path: '/weather/:state/:city',
       name: 'cityView',
       component: () => import('../views/CityView.vue'),
+      meta: {
+        title: 'Weather',
+      },
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const title = to.params.state
+    ? `${to.params.city}, ${to.params.state}`
+    : to.meta.title;
+  document.title = `${title} | The Weather`;
+  next();
 });
 
 export default router;
